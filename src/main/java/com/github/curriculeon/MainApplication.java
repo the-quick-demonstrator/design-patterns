9819081cb0510d439e7fbb5a8127010a3cbbe84e
git-leon <xleonhunter@gmail.com>
2023-05-05 11:53:04 -0400

    rw facade part 1

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,15 +1,20 @@
 package com.github.curriculeon;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainApplication {
     public static void main(String[] args) {
+        Integer numberOfGuesses = 0;
         final InputOutputFacade io = new InputOutputFacade();
         final Integer minInt = io.getIntegerInput("Enter a minimum value");
         final Integer maxInt = io.getIntegerInput("Enter a maximum value");
         final Integer randomValue = ThreadLocalRandom.current().nextInt(minInt, maxInt);
         while (true) {
             final Integer guessInt = io.getIntegerInput("Enter a guess between %s and %s", minInt, maxInt);
+            numberOfGuesses++;
             if (guessInt > randomValue) {
                 io.println("Guess a lower value");
             } else if (guessInt < randomValue) {
@@ -19,5 +24,12 @@ public class MainApplication {
                 break;
             }
         }
+        io.println("Number of Guesses: %s", numberOfGuesses);
+
+        final String currentProjectDirectory = System.getProperty("user.dir");
+        final String resourceDirectoryLocalPath = "/src/main/resources";
+        final String resourceDirectory = currentProjectDirectory + resourceDirectoryLocalPath;
+        final ReadWriteFacade rw = new ReadWriteFacade(resourceDirectory);
+        rw.write(numberOfGuesses.toString(), false);
     }
 }
---
+++ src/main/java/com/github/curriculeon/ReadWriteFacade.java
@@ -0,0 +1,73 @@
+package com.github.curriculeon;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadWriteFacade {
+    private final File file;
+
+    public ReadWriteFacade(String fileName) {
+        this(new File(fileName));
+    }
+
+    public ReadWriteFacade(File file) {
+        this.file = file;
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void write(String content, boolean append) {
+        final FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(file, append);
+            fileWriter.write(content);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String read() {
+        final StringBuilder contents = new StringBuilder();
+        try {
+            final Scanner scanner = new Scanner(this.file);
+            while(scanner.hasNextLine()) {
+                final String currentLine = scanner.nextLine();
+                contents.append(currentLine);
+                contents.append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return contents.toString();
+    }
+
+    public List<String> toLines() {
+        return Arrays.asList(read().split("\n"));
+    }
+
+    public String read(int lineNumber) {
+        return toLines().get(lineNumber);
+    }
+
+    public void replaceLine(int lineNumber, String contentToBeWritten) {
+        final StringBuilder result = new StringBuilder();
+        final List<String> lines = this.toLines();
+        lines.set(lineNumber, contentToBeWritten);
+        lines.forEach(line -> result.append(line).append("\n"));
+        this.write(result.toString().replaceAll("$\n", ""), false);
+    }
+    public void replaceAllOccurrences(String stringToReplace, String replacementString) {
+        final String contentToWrite = read().replaceAll(stringToReplace, replacementString);
+        this.write(contentToWrite, false);
+    }
+}
